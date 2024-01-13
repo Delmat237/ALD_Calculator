2f1c397920cb4e7ed0d868726e6f3bbe5484ed7e
Delmat237 <azangueleonel9@gmail.com>
2024-01-13 22:02:35 +0100

    Version 4 : les boutons de la premiere page sont toutes operationnelles

--- Calculator_App.py
+++ Calculator_App.py
@@ -1,49 +1,111 @@
 #Importation des modules
 import tkinter as tk
-from math import sin,cos,tan,sqrt,fabs,log,factorial,pi
+from math import sin,cos,tan,sqrt,fabs,log,factorial,pi,log10,sinh,cosh,tanh
 
 #Definitions des fonctions 
+def index(terme, expression):
+    indices = []
+    indice = expression.find(terme)
+
+    while indice != -1:
+        indices.append(indice)
+        indice= expression.find(terme, indice + 1)
+ 
+    return indices
+
+
+def evaluate(expression : str):
+    #Fonction permettant d'evaluer les expresions du champ d'entré
+    for car in ['√','sin','cos','tan','ln','Log','sh','ch','facto']:
+        if car in expression: #verifie si le car est dans l'expression
+            indices,i = index(car,expression),0 #si c'est le cas , on cherche toutes les positions
+            for p in indices:
+                if expression[p-1]  in ['*','+','-','/','(',')'] or  p == 0:
+                    continue
+                else :
+                    expression = expression[:p+i] +'*' + expression[p+i:]
+                    i += 1
+    return expression
+           
 def action(text_button):
-    global result
-    #association des fonctions oux boutons
+    global result,cursor,choix_DEG
+    #association des fonctions aux boutons
     if text_button == '=':
         if texte_variable.get() == 'Ans':
+            
             texte_variable.set(result)
         else:
+            #Gestion des erreurs
             try :
-                var  = texte_variable.get().replace("√","sqrt").replace("Abs","fabs").replace('ln','log').replace('facto','factorial').replace('π','pi')
+                var = evaluate(texte_variable.get()).replace("√","sqrt").replace("Abs","fabs").replace('ln','log')\
+                    .replace('facto','factorial').replace('π','pi').replace('sh','sinh').replace('ch','cosh')\
+                    .replace('th','tanh').replace('Log','log10')
                 result = eval(var)
                 texte_variable.set(result)
+                
+            except ZeroDivisionError:
+                texte_variable.set("Infinity")
+            except ValueError:
+                texte_variable.set("Value Error")
             except :
-                texte_variable.set("Math Error")
+                texte_variable.set("Syntax Error")
                        
     elif text_button == 'CE':
         texte_variable.set('')
+        
     elif text_button == '-->':
         frame.config(bg='black')
+        
     elif text_button == 'DEL':
-        if texte_variable.get() == 'Math Error':
+        if 'Math Error' in texte_variable.get():
             texte_variable.set('')
         else :
             texte_variable.set(texte_variable.get()[:-1])
-    elif text_button == 'ALPHA' or text_button == 'SHIFT':
+            
+    elif text_button in ['ALPHA','SHIFT','MORE','Exp']:
         pass
-    elif text_button == 'sin' or text_button == 'cos' or text_button == 'tan' \
-                or text_button == 'ln' or text_button == '√' or text_button == 'Abs' or text_button == 'facto':
-        texte_variable.set(texte_variable.get()+text_button+"(")
+    
+    elif text_button in [ 'sin', 'cos','tan', 'ln','√' ,'Abs', 'facto','Log']:
+        if hyp_value.get(): # Lorsque la touche hyp est activée ou non
+            mes = text_button.replace("sin","sh").replace("cos","ch").replace("tan","th")
+        else :
+            mes = text_button.replace("sh","sin").replace("ch","cos").replace("th","tan")
+            
+        texte_variable.set(texte_variable.get()+mes+"(")
+        
     elif text_button == '<--':
         entry.icursor(len(str(texte_variable))-1)
+        
     elif text_button == 'x²':
         texte_variable.set(texte_variable.get()+'²')
+        
     elif text_button == '2nd':
         frame.grid_forget()
-        frame2.grid(row = 2 ,column = 0)
+        frame2.grid(row = 2 ,column = 0,columnspan=5)
+        
     elif text_button == '1st':
         frame2.grid_forget()
-        frame.grid(row = 2 ,column = 0)
-    else :
-        texte_variable.set(texte_variable.get()+text_button)
+        frame.grid(row = 2 ,column = 0,columnspan=5)
+        
+    elif text_button == 'DEG':
+        choix = choix_DEG.get()
+        choix_DEG.set((choix+1)%3)
+        
+    elif text_button == 'hyp':
+        if hyp_value.get():
+            hyp_value.set(0)
+            for i,text in [(17,'sin'),(18,'cos'),(19,'tan')]:
+                buttons[i].config(text = text)
+        else :
+            hyp_value.set(1)
+            for i,text in [(17,'sh'),(18,'ch'),(19,'th')]:
+                buttons[i].config(text = text)
     
+   
+    else :
+        #texte_variable.set(texte_variable.get()+text_button)
+        entry.insert(cursor,text_button)
+   
     
 #Initialisation de la fenetre
 fen = tk.Tk()
@@ -55,7 +117,7 @@ entry = tk.Entry(frame1,textvariable = texte_variable,width=20,bd=5,justify='rig
 
 frame = tk.Frame(fen,bg='light yellow')
 
-frame2 = tk.Frame(fen,bg='light yellow',width = 200 , height = 200)
+frame2 = tk.Frame(fen,bg='light yellow',width = frame.winfo_width() , height = frame.winfo_height())
 
 # Bouton de la premiere page
 text_button= ['SHIFT','ALPHA','DEG','MORE','2nd',
@@ -73,7 +135,7 @@ text_button2 = ['SHIFT','ALPHA','x','y','1st',
               'Factor','and','or','xor','Not',
               'd/dx','∫','Det','Cross','Tr',
               'r','s','t','u','v',
-              '{}','〖[]〗','lim┬','(',')',
+              '{}','〖[]〗','lim','(',')',
               '7','8','9','False','True',
               '4','5','6','∑','∏',
               '1','2','3','∛','dy/dx',
@@ -88,20 +150,40 @@ tk.Label(fen,text=' \n' ).grid(row=1,column=0,columnspan=5)
 frame.grid(row=2,column=0, columnspan=5)
 row ,column = 2,0
 result = None
+cursor = tk.END
+
+#Creation des radiobuttons pour les touches hyp et DEG
+choix_DEG = tk.IntVar()
+for i,text in enumerate(['D','r','G']):
+    DEG_button = tk.Radiobutton(fen,text =text ,value = i , variable  = choix_DEG)      
+    DEG_button.grid(row = 1, column =i)
+    
+hyp_value = tk.IntVar()
+hyp_button = tk.Checkbutton(fen,text = 'hyp',variable = hyp_value)
+hyp_button.grid(row=1, column= 3)
 
 #creation des boutons
 for i,text in enumerate(text_button):
-    buttons[i]=tk.Button(frame,text = text, bg = 'dark grey',fg='navy',width=5,bd=1,command = lambda m = text : action(m))
+    buttons[i]=tk.Button(frame,text = text, bg = 'dark grey',fg='navy',width=5,bd=1)
+    buttons[i]['command'] = lambda m = buttons[i].cget('text') : action(m)
+    
+    #mise en forme des boutons
     if text == 'SHIFT':
         buttons[i].config(bg='yellow')
+        
     elif text == 'ALPHA':
         buttons[i].config(bg='green',fg="white")
+        
     elif text == 'DEG':
         buttons[i].config(bg='royal blue',fg="white")
+        
     elif text == 'CE' or text == 'DEL':
         buttons[i].config(bg='orange')
-    elif text == '*' or text == '/' or text == '+' or text == '-':
+        
+    elif text in ['*' ,'/' ,'+' ,'-']:
         buttons[i].config(bg='pink',fg="black")
+    
+                
     buttons[i].grid(row = row,column=column,padx =5,pady =10)
     column += 1
     if not  column % 5:
@@ -111,22 +193,27 @@ for i,text in enumerate(text_button):
 row ,column = 2,0
 
 for i,text in enumerate(text_button2):
-    buttons2[i]=tk.Button(frame2,text = text, bg = 'dark grey',fg='navy',width=5,bd=1,command = lambda m = text : action(m))
+    buttons2[i]=tk.Button(frame2,text = text, bg = 'dark grey',fg='navy',width=5,bd=1)
     buttons2[i].grid(row = row,column=column,padx =5,pady =10)
+    buttons2[i]['command'] = lambda m = buttons2[i].cget('text') : action(m)
+    
     if text == 'SHIFT':
             buttons2[i].config(bg='yellow')
+            
     elif text == 'ALPHA':
         buttons2[i].config(bg='green',fg="white")
+        
     elif text == 'True'or text == 'False':
         buttons2[i].config(bg='royal blue',fg="white")
+        
     elif text == 'CE' or text == 'Not':
         buttons2[i].config(bg='orange')
-    elif text == '*' or text == '/' or text == '+' or text == '-':
-        buttons2[i].config(bg='pink',fg="black")
+        
     column += 1
     if not  column % 5:
         row += 1
         column = 0
-        
+
+
 #Mise à jour de la fenetre
 fen.mainloop()
